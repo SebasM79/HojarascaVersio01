f48d4f078cddf93ed22a75c7f997b7c4c50f0c04
SebasM79 <sebamussi.dev@hotmail.com>
2024-11-19 12:49:34 -0300

    inicio app

---
+++ database/migrations/2024_11_18_000000_create_plantas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plantas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->date('fecha_ingreso');
+            $table->decimal('importe', 10, 2);
+            $table->boolean('planta_activa');
+            $table->text('descripcion');
+            $table->string('mail_origen');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plantas');
+    }
+};
